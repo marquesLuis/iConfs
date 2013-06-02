c61827a28562007e8621faf7187d9b3a472f7e23
martalidon <m.lidon@campus.fct.unl.pt>
2013-06-02 13:52:56 +0100

    Networking.m update

---
+++ iConfs-iOS/Networking.m
@@ -0,0 +1,13 @@
+//
+//  Networking.m
+//  iConfs-iOS
+//
+//  Created by Marta Lidon on 01/06/13.
+//  Copyright (c) 2013 FCTUNL. All rights reserved.
+//
+
+#import "Networking.h"
+
+@implementation Networking
+
+@end
