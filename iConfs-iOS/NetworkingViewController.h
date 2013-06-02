1891bc937a543a9937bfcb7762c18e042add3e5f
martalidon <m.lidon@campus.fct.unl.pt>
2013-06-02 13:52:08 +0100

    NetworkingVC.h update

---
+++ iConfs-iOS/NetworkingViewController.h
@@ -0,0 +1,19 @@
+//
+//  NetworkingViewController.h
+//  iConfs-iOS
+//
+//  Created by Marta Lidon on 01/06/13.
+//  Copyright (c) 2013 FCTUNL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "sqlite3.h"
+#import "Networking.h"
+
+
+
+
+@interface NetworkingViewController : UIViewController
+
+@end
