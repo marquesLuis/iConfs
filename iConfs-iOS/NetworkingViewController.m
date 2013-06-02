9be3098c20dfac9039abf0ec939c58e075797e5d
martalidon <m.lidon@campus.fct.unl.pt>
2013-06-02 13:52:21 +0100

    NetworkingVC.m update

---
+++ iConfs-iOS/NetworkingViewController.m
@@ -0,0 +1,38 @@
+//
+//  NetworkingViewController.m
+//  iConfs-iOS
+//
+//  Created by Marta Lidon on 01/06/13.
+//  Copyright (c) 2013 FCTUNL. All rights reserved.
+//
+
+#import "NetworkingViewController.h"
+
+@interface NetworkingViewController ()
+
+@end
+
+@implementation NetworkingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
