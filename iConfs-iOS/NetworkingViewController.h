020514caf7e13826dc9676b5b6870bb53aa6e5c8
martalidon <m.lidon@campus.fct.unl.pt>
2013-06-02 15:50:53 +0100

    networkingVC.h update

--- iConfs-iOS/NetworkingViewController.h
+++ iConfs-iOS/NetworkingViewController.h
@@ -15,5 +15,13 @@
 
 
 @interface NetworkingViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *personName;
+@property (strong, nonatomic) IBOutlet UILabel *networkingTitle;
+@property (strong, nonatomic) NSString * networkingDescriptionContent;
+@property (strong, nonatomic) NSString * photoPath;
+@property (strong, nonatomic) IBOutlet UITextView *networkingDescription;
+@property (strong, nonatomic) IBOutlet UIImageView *personPhoto;
+@property int numNetworking;
+
 
 @end
