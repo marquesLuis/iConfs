a1190de6538270f3df37644d01bc25c7f4e4488b
martalidon <m.lidon@campus.fct.unl.pt>
2013-06-02 13:53:18 +0100

    Person.m update

---
+++ iConfs-iOS/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  iConfs-iOS
+//
+//  Created by Marta Lidon on 01/06/13.
+//  Copyright (c) 2013 FCTUNL. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
