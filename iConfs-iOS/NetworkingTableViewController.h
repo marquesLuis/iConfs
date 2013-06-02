5e1698f2f9f6e5a52a32b4caa7558d081597b936
martalidon <m.lidon@campus.fct.unl.pt>
2013-06-02 13:51:35 +0100

    NetworkingTableVC update

---
+++ iConfs-iOS/NetworkingTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  NetworkingTableViewController.h
+//  iConfs-iOS
+//
+//  Created by Marta Lidon on 01/06/13.
+//  Copyright (c) 2013 FCTUNL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NetworkingViewController.h"
+#import "Person.h"
+
+@interface NetworkingTableViewController : UITableViewController
+
+@end
