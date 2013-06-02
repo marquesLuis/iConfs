73520e900790cd03c26ba466a9a9072516ede829
martalidon <m.lidon@campus.fct.unl.pt>
2013-06-02 13:52:40 +0100

    Networking.h update

---
+++ iConfs-iOS/Networking.h
@@ -0,0 +1,20 @@
+//
+//  Networking.h
+//  iConfs-iOS
+//
+//  Created by Marta Lidon on 01/06/13.
+//  Copyright (c) 2013 FCTUNL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Networking : NSObject
+
+    
+@property (nonatomic, strong) NSString * text;
+@property (nonatomic, strong) NSString * title;
+@property (nonatomic, strong) NSString * date;
+@property (nonatomic, strong) NSArray * area;
+@property (nonatomic, strong) NSString * personID;
+
+@end
