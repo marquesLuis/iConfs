63cc87347fd82a6b0dfa0d41f929eca4658576be
martalidon <m.lidon@campus.fct.unl.pt>
2013-06-05 19:20:00 +0100

    personal program

--- iConfs-iOS/PersonalProgramTKCalendarDayViewController.m
+++ iConfs-iOS/PersonalProgramTKCalendarDayViewController.m
@@ -9,6 +9,8 @@
 #import "PersonalProgramTKCalendarDayViewController.h"
 
 @interface PersonalProgramTKCalendarDayViewController ()<TKCalendarDayViewDelegate> {
+    NSString * beginDate;
+    NSString * endDate;
 }
 
 @end
@@ -24,23 +26,46 @@
     return self;
 }
 
+/** 
+ */
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.title = @"Personal Program";
     
     
+    [self getBeginAndEnd];
     
-    NSString *dateStr = @"2013-06-03 07:26";
     NSDateFormatter *dateFormatter1 = [[NSDateFormatter alloc] init];
-    [dateFormatter1 setDateFormat:@"yyyy-MM-dd hh:mm"];
+    [dateFormatter1 setDateFormat:@"yyyy-MM-dd"];
     dateFormatter1.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
-    NSDate *date = [dateFormatter1 dateFromString:dateStr];
-   // self.dayView.date = date;
+    NSDate *date = [dateFormatter1 dateFromString:beginDate];
+    self.dayView.date = date;
 }
 
--(void)initializeEvents{
+
+-(void)getBeginAndEnd{
+    sqlite3_stmt *statement;
+    sqlite3 *db;
+    NSArray *path = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docPath = [path objectAtIndex:0];
+    NSString *dbPathString = [docPath stringByAppendingPathComponent:@"calendar.db"];
     
+    if (sqlite3_open([dbPathString UTF8String], &db)==SQLITE_OK) {
+        
+        NSString *querySql = [NSString stringWithFormat:@"SELECT * FROM CALENDAR"];
+        const char* query_sql = [querySql UTF8String];
+        
+        if (sqlite3_prepare(db, query_sql, -1, &statement, NULL)==SQLITE_OK) {
+            while (sqlite3_step(statement)==SQLITE_ROW) {
+                beginDate = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(statement, 1)];
+                endDate = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(statement, 2)];
+                
+            }
+        }
+        sqlite3_close(db);
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -52,16 +77,6 @@
 #pragma mark TKCalendarDayViewDelegate
 - (NSArray *) calendarDayTimelineView:(TKCalendarDayView*)calendarDayTimeline eventsForDate:(NSDate *)eventDate{
 
-   /*
-     //Events
-     [self createOrOpenDB:"CREATE TABLE IF NOT EXISTS EVENTS( ID INTEGER PRIMARY KEY AUTOINCREMENT, TITLE TEXT, DESCRIPTION TEXT, SERVER_ID INTEGER, KIND TEXT, BEGIN TEXT, END TEXT, DURATION INTEGER, DATE TEXT, LOCATION_ID INTEGER, SPEAKER_ID INTEGER, KEYNOTE INTEGER,  LOCAL_ID INTEGER)" WithName:@"events.db"];
-     
-     [self createOrOpenDB:"CREATE TABLE IF NOT EXISTS EVENTS_STATUS( ID INTEGER PRIMARY KEY AUTOINCREMENT, LAST_DATE TEXT, LAST_ID INTEGER, LAST_REMOVED INTEGER)" WithName:@"events_status.db"];
-     
-     //Attending
-     [self createOrOpenDB:"CREATE TABLE IF NOT EXISTS ATTENDING( ID INTEGER PRIMARY KEY AUTOINCREMENT, SESSION_ID INTEGER)" WithName:@"attending.db"];
-     */
-    
     sqlite3 *db;
     NSMutableArray *events = [[NSMutableArray alloc]init];
     NSLog(@"fill day!");
@@ -89,23 +104,33 @@
                 while (sqlite3_step(myStatment)==SQLITE_ROW) {
                     NSLog(@"fill day!3");
 
+                    /** [self createOrOpenDB:"CREATE TABLE IF NOT EXISTS ATTENDING( ID INTEGER PRIMARY KEY AUTOINCREMENT, SESSION_ID INTEGER, SERVER_ID INTEGER)" WithName:@"attending.db"];
+                     
+                     //Events
+                     [self createOrOpenDB:"CREATE TABLE IF NOT EXISTS EVENTS( ID INTEGER PRIMARY KEY AUTOINCREMENT, TITLE TEXT, DESCRIPTION TEXT, SERVER_ID INTEGER, KIND TEXT, BEGIN TEXT, END TEXT, DATE TEXT, SPEAKER_ID INTEGER, KEYNOTE INTEGER,  LOCAL_ID INTEGER)" WithName:@"events.db"];
+                     */
+                    
                     Event * e = [[Event alloc]init];
-                    NSString *title = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 3)];
-                    NSString *description = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 4)];
-                    NSString *kind = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 6)];
-                    NSString *dateBegin = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 7)];
-                    NSString *dateEnd = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 8)];
+                    NSString *title = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 4)];
+                    NSString *description = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 5)];
+                    NSString *kind = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 7)];
+                    NSString *dateBegin = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 8)];
+                    NSString *dateEnd = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 9)];
                     NSString *date = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 10)];
-                    NSString *local = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 11)];
+                    NSString *local = [[NSString alloc]initWithUTF8String:(const char *)sqlite3_column_text(myStatment, 13)];
                     NSLog(title);
+                    NSLog(dateBegin);
+                    NSLog(dateEnd);
+                    NSLog(date);
 
                     e.title = title;
                     e.descrption = description;
                     e.kind = kind;
                     e.begin = [date stringByAppendingString:dateBegin];
+                    NSLog(e.begin);
                     e.end = [date stringByAppendingString:dateEnd];
+                                        NSLog(e.end);
                     e.localID = local;
-                    [events addObject:e];
                     [events addObject:e];
                 }
             
@@ -127,8 +152,11 @@
         
 		event.locationLabel.text = [self getLocal:ev.localID];
 		event.startDate = [self convertNSStringToNSDate:ev.begin];
+        
 		
 		event.endDate = [self convertNSStringToNSDate:ev.end];
+        NSLog(@"date:");
+        NSLog(@"%@, %@, %@", event.startDate, event, ev);
         
 		[ret addObject:event];
 		
@@ -165,8 +193,8 @@
 -(NSDate*)convertNSStringToNSDate:(NSString*)date{
     NSString *beginDateDB = date;
     NSDateFormatter *dateFormatter1 = [[NSDateFormatter alloc] init];
-    [dateFormatter1 setDateFormat:@"yyyy-MM-dd hh:mm"];
-    dateFormatter1.timeZone = [NSTimeZone defaultTimeZone ];
+    [dateFormatter1 setDateFormat:@"yyyy-MM-dd HH:mm"];
+  //  dateFormatter1.timeZone = [NSTimeZone defaultTimeZone ];
     return  [dateFormatter1 dateFromString:beginDateDB];
 }
 
