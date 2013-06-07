d782ab0fcd6fdd0a8afe98145d75840fd94a0dad
martalidon <m.lidon@campus.fct.unl.pt>
2013-06-07 20:59:41 +0100

    add event

---
+++ iConfs-iOS/Author.h
@@ -0,0 +1,13 @@
+//
+//  Author.h
+//  iConfs-iOS
+//
+//  Created by Marta Lidon on 06/06/13.
+//  Copyright (c) 2013 FCTUNL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Author : NSObject
+
+@end
---
+++ iConfs-iOS/EventUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventUIViewController.h
+//  iConfs-iOS
+//
+//  Created by Marta Lidon on 06/06/13.
+//  Copyright (c) 2013 FCTUNL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventUIViewController : UIViewController
+
+@end
